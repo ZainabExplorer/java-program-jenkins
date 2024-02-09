f2cafa8d1d09046f7162d3edf183cc25ad95351f
ZainabExplorer <159520651+ZainabExplorer@users.noreply.github.com>
2024-02-09 13:32:51 +0530

    2 commit

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
         public static void main(String[] args){
             System.out.println("I am Java program");
         System.out.println("heyyyyyyyy");
+                System.out.println("mai hun");
+
         
     
     
